31278f885731236432f055f8847ee3c29d910378
SHUBHANKAR SHARMA <shubhankarsharma22@gmail.com>
2023-07-19 21:42:37 +0530

    making MONGODB_URI ready for deployment

--- database/db.js
+++ database/db.js
@@ -8,8 +8,8 @@ dotenv.config();
 const DBConnection = async()=>{
     const USERNAME = process.env.DB_USERNAME;
     const PASSWORD = process.env.DB_PASSWORD;
-
-    const MONGODB_URI=`mongodb://${USERNAME}:${PASSWORD}@ac-uzfcvg1-shard-00-00.qqj967k.mongodb.net:27017,ac-uzfcvg1-shard-00-01.qqj967k.mongodb.net:27017,ac-uzfcvg1-shard-00-02.qqj967k.mongodb.net:27017/?ssl=true&replicaSet=atlas-676ojb-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const mongoDB_URI = process.env.MONGODB_URI;
+    const MONGODB_URI=`${mongoDB_URI}`;
 
     try {
         await mongoose.connect(MONGODB_URI,{useNewUrlParser: true});
