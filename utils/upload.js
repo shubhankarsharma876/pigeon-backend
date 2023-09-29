4bf1c98d2ecd08f6e1a88bb74e6403124663fad8
SHUBHANKAR SHARMA <71379135+shubhankarsharma876@users.noreply.github.com>
2023-09-29 10:33:58 +0530

    comment adding

--- utils/upload.js
+++ utils/upload.js
@@ -1,5 +1,5 @@
 import multer from "multer";
-
+//For the easy handling of multipart/form data that is used when file uploading is done.
 
 const upload = multer({dest: 'uploads' })
 export default upload;
